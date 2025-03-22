adc0cb88baad4b8b2cc5ef2fa5c9899f35ef1cb5
brt4c3 <thankyouryugo12345@gmail.com>
2025-03-22 20:37:08 +0900

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.kotlin.compose)
+    //id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -10,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.example.radiowaveproject"
         minSdk = 31
-        targetSdk = 35
+
         versionCode = 1
         versionName = "1.0"
 
@@ -18,7 +20,7 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -28,61 +30,63 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     buildFeatures {
         compose = true
+        viewBinding = false
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.6.0"
     }
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+    buildToolsVersion = "35.0.0"
+    ndkVersion = "25.2.9519653"
 }
 
 dependencies {
+    // Core AndroidX dependencies
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui.tooling.preview)
 
+    // Jetpack Compose BOM (Bill of Materials)
+    implementation(platform(libs.androidx.compose.bom))
+
+    // Compose dependencies
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.foundation)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.support.annotations)
+    implementation(libs.androidx.legacy.support.v13)
+    implementation(libs.material)
+    implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.1")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.4.0-alpha10")
+
+
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
-//pluginManagement {
-//    repositories {
-//        google()
-//        mavenCentral()
-//        gradlePluginPortal()
-//    }
-//}
-
-//dependencyResolutionManagement {
-//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-//    repositories {
-//        google()
-//        mavenCentral()
-//    }
-//    versionCatalogs {
-//        create("libs") {
-//            from(files("gradle/libs.versions.toml"))
-//        }
-//    }
-//}
-
-//rootProject.name = "RadioWaveProject"
-//include(":app")
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,12 @@ plugins {
     //alias(libs.plugins.kotlin.android) apply false
     //alias(libs.plugins.kotlin.compose) apply false
     id("com.android.application") version "8.9.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    //id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    //id("org.jetbrains.kotlin.plugin.compose.gradle.plugin") version "2.0.21" apply false
+
+    //id("com.google.devtools.ksp") version "2.0.21-1.0.21" apply false
 
 }
 
